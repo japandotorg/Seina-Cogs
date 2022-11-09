db96053b75d5a7f13c2b44d649466df555eb9d6e
{{ github.actor }} <japandotorg@users.noreply.github.com>
2022-11-09 15:49:18 +0000

    Style Reformatting

--- rainbowrole/core.py
+++ rainbowrole/core.py
@@ -23,16 +23,12 @@ SOFTWARE.
 """
 
 import logging
-from typing import (
-    Optional,
-    Union,
-    Literal,
-)
+from typing import Literal, Optional, Union
 
 import discord
 from discord.ext import tasks
-from redbot.core.bot import Red # type: ignore
-from redbot.core import commands, Config # type: ignore
+from redbot.core import Config, commands  # type: ignore
+from redbot.core.bot import Red  # type: ignore
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -51,32 +47,32 @@ COLORS = [
     (243, 56, 255),
 ]
 
+
 class RainbowRole(BaseCog):  # type: ignore
     """
     Create and manage a Rainbow Role in your server.
     """
-    
+
     __version__ = "0.1.0"
     __author__ = "inthedark.org#0666"
-    
-    def __init__(
-        self,
-        bot: Red
-    ):
+
+    def __init__(self, bot: Red):
         self.bot: Red = bot
-        self.config: Config = Config.get_conf(self, identifier=759180080328081450, force_registration=True)
+        self.config: Config = Config.get_conf(
+            self, identifier=759180080328081450, force_registration=True
+        )
         self.log = logging.LoggerAdapter(log, {"version": self.__version__})
-        
+
         default_guild = {
             "rainbow_role": None,
             "toggled": False,
         }
-        
+
         self.config.register_guild(**default_guild)
-        
+
         self.color = 0
         self.change_color.start()
-        
+
     async def red_get_data_for_user(self, *, user_id: int):
         """
         Nothing to get.
@@ -88,7 +84,7 @@ class RainbowRole(BaseCog):  # type: ignore
         Nothing to delete.
         """
         return {}
-        
+
     def format_help_for_context(self, ctx: commands.Context):
         pre_processed = super().format_help_for_context(ctx)
         n = "\n" if "\n\n" not in pre_processed else ""
@@ -98,18 +94,18 @@ class RainbowRole(BaseCog):  # type: ignore
             f"Cog Version: **{self.__version__}**",
         ]
         return "\n".join(text)
-    
+
     @tasks.loop(seconds=90.0)
     async def change_color(self):
         """
         The task that is responsible for the color change every x seconds.
         """
         for guild in self.bot.guilds:
-            
+
             toggle = self.config.guild(guild).toggle()
             if not toggle:
                 return
-            
+
             rainbow_role = self.config.guild(guild).rainbow_role()
             if rainbow_role:
                 try:
@@ -122,39 +118,35 @@ class RainbowRole(BaseCog):  # type: ignore
                     )
                 except:
                     self.log.info("Oops! Something went wrong.", exc_info=True)
-                    
+
         self.color = self.color + 1 if self.color + 1 <= 7 else 0
-        
+
     @change_color.before_loop
     async def before_oscallha(self):
         await self.bot.wait_until_red_ready()
-        
+
     def cog_unload(self):
         self.bot.loop.create_task(self.change_color.stop())
-    
+
     @commands.group()
     @commands.guildowner_or_permissions(administrator=True)
     async def set(self, ctx: commands.Context):
         """Group commands for the rainbow role settings."""
         if ctx.invoked_subcommand is None:
             await ctx.send_help("set")
-    
+
     @set.command()
     @commands.guildowner_or_permissions(administrator=True)
-    async def toggle(
-        self, 
-        ctx: commands.Context, 
-        true_or_false: Optional[bool] = None
-    ):
+    async def toggle(self, ctx: commands.Context, true_or_false: Optional[bool] = None):
         """
         Toggle the rainbow role task.
         """
         if true_or_false is None:
             await ctx.send("The `true_or_false` option is a required argument.")
-            
+
         await self.config.guild(ctx.guild).toggle(true_or_false)
         return await ctx.tick()
-    
+
     @set.command()
     @commands.guildowner_or_permissions(administrator=True)
     async def role(
@@ -166,7 +158,6 @@ class RainbowRole(BaseCog):  # type: ignore
         Set your rainbow role using this command.
         """
         role = ctx.guild.get_role(getattr(role, "id", role))
-        await self.config.guild(ctx.guild).rainbow_role.set(role.id) # type: ignore
+        await self.config.guild(ctx.guild).rainbow_role.set(role.id)  # type: ignore
         await ctx.tick()
         await ctx.send(f"Rainbow role set to `{role}`")
-        
