7f0a3800d39ab270d4ad172da23344f21656c207
Lemon Rose <yash.kul69@gmail.com>
2022-11-09 21:17:04 +0530

    init + license

--- rainbowrole/core.py
+++ rainbowrole/core.py
@@ -1,7 +1,32 @@
+"""
+MIT License
+
+Copyright (c) 2022-present japandotorg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import logging
 from typing import (
     Optional,
     Union,
+    Literal,
 )
 
 import discord
@@ -9,7 +34,11 @@ from discord.ext import tasks
 from redbot.core.bot import Red # type: ignore
 from redbot.core import commands, Config # type: ignore
 
+BaseCog = getattr(commands, "Cog", object)
+
 log: logging.Logger = logging.getLogger("red.seinacogs.rainbowrole")
+
+RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
 
 COLORS = [
     (255, 62, 62),
@@ -22,7 +51,7 @@ COLORS = [
     (243, 56, 255),
 ]
 
-class RainbowRole(commands.Cog):
+class RainbowRole(BaseCog):  # type: ignore
     """
     Create and manage a Rainbow Role in your server.
     """
@@ -47,6 +76,18 @@ class RainbowRole(commands.Cog):
         
         self.color = 0
         self.change_color.start()
+        
+    async def red_get_data_for_user(self, *, user_id: int):
+        """
+        Nothing to get.
+        """
+        return {}
+
+    async def red_delete_data_for_user(self, *, requester: RequestType, user_id: int):
+        """
+        Nothing to delete.
+        """
+        return {}
         
     def format_help_for_context(self, ctx: commands.Context):
         pre_processed = super().format_help_for_context(ctx)
