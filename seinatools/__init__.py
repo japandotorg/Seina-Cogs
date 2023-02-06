679b50714a4ef519fcc76741292de76ea6e7fb97
Lemon Rose <yash.kul69@gmail.com>
2023-02-06 21:07:16 +0530

    [SeinaTools] dpy2 async changes

--- seinatools/__init__.py
+++ seinatools/__init__.py
@@ -34,7 +34,7 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     cog = SeinaTools(bot)
-    bot.add_cog(cog)
-    asyncio.create_task(cog.initialize())
+    await bot.add_cog(cog)
+    asyncio.create_task(cog.cog_load())
