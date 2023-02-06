b6b876d654755d9870b5f47803a4d1f30fb1ecaa
{{ github.actor }} <japandotorg@users.noreply.github.com>
2023-02-06 16:01:54 +0000

    Style Reformatting

--- seinatools/core.py
+++ seinatools/core.py
@@ -392,8 +392,7 @@ class SeinaTools(BaseCog):  # type: ignore
         view = SetApiView("removebg", keys)
         if await ctx.embed_requested():
             embed: discord.Embed = discord.Embed(
-                description=message, 
-                color=await ctx.embed_color()
+                description=message, color=await ctx.embed_color()
             )
             await ctx.send(embed=embed, view=view)
         else:
