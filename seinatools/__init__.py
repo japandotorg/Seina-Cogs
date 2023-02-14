a95caeb3424aa57475ba81ae81143432e7f4b28c
Lemon Rose <yash.kul69@gmail.com>
2023-02-15 00:44:09 +0530

    [SeinaTools] Fix

--- seinatools/__init__.py
+++ seinatools/__init__.py
@@ -23,15 +23,10 @@ SOFTWARE.
 """
 
 import asyncio
-import json
-from pathlib import Path
 
 from redbot.core.bot import Red  # type: ignore
 
 from .core import SeinaTools
-
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red):
