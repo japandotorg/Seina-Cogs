c6467c4fb0027fd819b32ba91c5cecf62b4f1c47
Lemon Rose <yash.kul69@gmail.com>
2023-03-05 16:34:18 +0530

    [SeinaTools] fix block formatting

--- seinatools/core.py
+++ seinatools/core.py
@@ -690,12 +690,7 @@ class SeinaTools(BaseCog):  # type: ignore
                 )
         embed.add_field(
             name="Downloads",
-            value=(
-                "```prolog\n"
-                f"Total Downloads          : {resp['downloads']}\n"
-                f"Latest Version Downloads : {resp['version_downloads']:,}\n",
-                "```",
-            ),
+            value=f"```prolog\nTotal Downloads          : {resp['downloads']}\nLatest Version Downloads : {resp['version_downloads']:,}\n```",
             inline=False,
         )
         await ctx.send(embed=embed)
