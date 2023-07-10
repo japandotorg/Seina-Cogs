2e0f6c382f4a9575b1794bf21a2d1e496be65a8f
Lemon Rose <yash.kul69@gmail.com>
2023-07-10 15:50:27 +0530

    [BattleRoyale] make `_cooldown` a stiaticmethod

--- battleroyale/core.py
+++ battleroyale/core.py
@@ -47,7 +47,7 @@ from redbot.core.utils.views import SimpleMenu
 from .constants import SWORDS
 from .converters import EmojiConverter
 from .game import Game
-from .utils import _cooldown, _get_attachments, exceptions
+from .utils import _get_attachments, exceptions
 from .views import JoinGameView
 
 log: logging.Logger = logging.getLogger("red.seina.battleroyale")
@@ -124,6 +124,13 @@ class BattleRoyale(commands.Cog):
         for user in users:
             count = await self.config.user(user).get_raw(_type)
             await self.config.user(user).set_raw(_type, value=count + 1)
+    
+    @staticmethod
+    def _cooldown(ctx: commands.Context) -> Optional[commands.Cooldown]:
+        if ctx.author.id in ctx.bot.owner_ids:
+            return None
+        cooldown = ctx.cog.config.cooldown()
+        return commands.Cooldown(1, int(cooldown))
 
     @exceptions
     async def generate_image(
