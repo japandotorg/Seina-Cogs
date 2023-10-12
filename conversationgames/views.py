785dd68c08db0279e5212a257f0e4501c14fd3c1
Lemon Rose <yash.kul69@gmail.com>
2023-10-12 19:19:00 +0530

    [Fix Merge Conflict]

--- conversationgames/views.py
+++ conversationgames/views.py
@@ -2,12 +2,8 @@ import functools
 from typing import Callable, Dict, List, Optional, Union
 
 import discord
-from redbot.core import commands
-<<<<<<< HEAD
-=======
 from redbot.core.bot import Red
-from typing_extensions import Self
->>>>>>> b973310521314f4536f08711eb0d9ef3d8254592
+from redbot.core import commands
 
 
 class BaseLanguageOptions:
