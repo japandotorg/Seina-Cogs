b1b4ab36e2e3542584e9d126dde024c940cfd31d
Lemon Rose <yash.kul69@gmail.com>
2023-10-13 13:15:16 +0530

    [ThreadOpener] license

--- threadopener/core.py
+++ threadopener/core.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2021-present Kuro-Rui
+Copyright (c) 2021-present japandotorg
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
