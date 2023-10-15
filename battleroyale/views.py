db9c169afbcf92a8cb33453323e0595752258348
Lemon Rose <yash.kul69@gmail.com>
2023-10-15 19:35:23 +0530

    [BattleRoyale] shift to name instead

--- battleroyale/views.py
+++ battleroyale/views.py
@@ -123,7 +123,7 @@ class RemainingPlayerView(discord.ui.View):
         self: RemainingPlayerButton, interaction: discord.Interaction[Red]
     ) -> None:
         remaining_player_str = humanize_list(
-            [m.display_name for m in sorted(self.view.remaining, key=lambda m: m.display_name)]
+            [m.name for m in sorted(self.view.remaining, key=lambda m: m.name)]
         )
         remaining_players_str = (
             f"{remaining_player_str[:4000]}..."
@@ -132,7 +132,7 @@ class RemainingPlayerView(discord.ui.View):
         )
         embed: discord.Embed = discord.Embed.from_dict(
             {
-                "description": f"{remaining_players_str}.",
+                "description": f"**Remaining Players:**\n{remaining_players_str}.",
                 "color": int(self.view.color),
             }
         )
