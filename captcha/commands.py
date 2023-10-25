08e15bcd25807342549d03dabd73710d76f09809
Lemon Rose <yash.kul69@gmail.com>
2023-10-26 00:59:37 +0530

    [Captcha] fix imports

--- captcha/commands.py
+++ captcha/commands.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import Any, Optional
+from typing import Any, Optional, Dict
 
 import discord
 from redbot.core import commands
