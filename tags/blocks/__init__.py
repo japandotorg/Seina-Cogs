46f03d70b9602e1a4a1aa8a6ddc96261cad1884a
japandotorg <yash.kul69@gmail.com>
2024-06-25 08:44:03 +0530

    [Tags] import the new block in `blocks.__init__`

--- tags/blocks/__init__.py
+++ tags/blocks/__init__.py
@@ -25,6 +25,7 @@ SOFTWARE.
 
 from typing import Tuple
 
+from .allowed_mentions import AllowedMentionsBlock as AllowedMentionsBlock
 from .comment import CommentBlock as CommentBlock
 from .customcom import ContextVariableBlock as ContextVariableBlock
 from .customcom import ConverterBlock as ConverterBlock
