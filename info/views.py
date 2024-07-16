a1143922c9274d866b6d8a261c1ddc0143e65f68
japandotorg <yash.kul69@gmail.com>
2024-07-16 08:55:56 +0530

    [Info] fix enabled

--- info/views.py
+++ info/views.py
@@ -480,7 +480,7 @@ class CommandView(discord.ui.View):
                 {}
                 """.format(
                     command.hidden,
-                    command.is_enabled(),
+                    command.enabled,
                     "**Parent**: {}".format(humanize_list(parents) if len(parents) > 1 else ""),
                 )
             ),
