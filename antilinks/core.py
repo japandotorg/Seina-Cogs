bf2e4565d5a0450e985480a6308912bb9c2eb6d7
Lemon Rose <yash.kul69@gmail.com>
2025-02-12 07:36:15 +0530

    [AntiLinks] fix logs channel

--- antilinks/core.py
+++ antilinks/core.py
@@ -24,7 +24,16 @@ SOFTWARE.
 """
 
 import re
-from typing import Dict, Final, List, Literal, Match, Optional, Pattern, Union
+from typing import (
+    Dict,
+    Final,
+    List,
+    Literal,
+    Match,
+    Optional,
+    Pattern,
+    Union,
+)
 
 import discord
 from red_commons.logging import RedTraceLogger, getLogger
@@ -35,7 +44,9 @@ from redbot.core.utils.views import SimpleMenu
 
 log: RedTraceLogger = getLogger("red.seinacogs.antilinks")
 
-RequestType = Literal["discord_deleted_user", "owner", "user", "user_strict"]
+RequestType = Literal[
+    "discord_deleted_user", "owner", "user", "user_strict"
+]
 
 LINKS: Pattern[str] = re.compile(
     r"(\|\|)?(([\w]+:)?\/\/)?(([\d\w]|%[a-fA-f\d]{2,2})+(:([\d\w]|%[a-fA-f\d]{2,2})+)?@)?([\d\w]?[-\d\w]{0,253}[\d\w]\.)+[\w]{2,63}(:[\d]+)?(\/([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)*(\?(&?([-+_~.\d\w]|%[a-fA-f\d]{2,2})=?)*)?(#([-+_~.\d\w]|%[a-fA-f\d]{2,2})*)?(\|\|)?"  # type: ignore
@@ -53,7 +64,9 @@ class AntiLinks(commands.Cog):
     def __init__(self, bot: Red) -> None:
         self.bot: Red = bot
 
-        self.config: Config = Config.get_conf(self, identifier=69420, force_registration=True)
+        self.config: Config = Config.get_conf(
+            self, identifier=69420, force_registration=True
+        )
         default_guild: Dict[str, Union[Optional[int], List]] = {
             "report_channel": None,
             "role": [],
@@ -99,8 +112,12 @@ class AntiLinks(commands.Cog):
             await self.config.guild(ctx.guild).report_channel.clear()
             await ctx.send("Message transfer channel turned off.")
             return
-        await self.config.guild(ctx.guild).report_channel.set(channel.id)
-        await ctx.send(f"Message transfer channel set to: {channel.mention}.")
+        await self.config.guild(ctx.guild).report_channel.set(
+            channel.id
+        )
+        await ctx.send(
+            f"Message transfer channel set to: {channel.mention}."
+        )
 
     @_anti.group(name="whitelist")
     async def _whitelist(self, ctx: commands.Context) -> None:
@@ -123,7 +140,9 @@ class AntiLinks(commands.Cog):
                 await ctx.send("`Roles` is a required argument.")
                 return
 
-            async with self.config.guild(ctx.guild).role() as role_list:
+            async with self.config.guild(
+                ctx.guild
+            ).role() as role_list:
                 for role in roles:
                     if add_or_remove.lower() == "add":
                         if not role.id in role_list:
@@ -147,19 +166,30 @@ class AntiLinks(commands.Cog):
         whitelisted_roles = await guild_config.role()
 
         if not whitelisted_roles:
-            await ctx.send("There are no whitelisted roles in this server.")
+            await ctx.send(
+                "There are no whitelisted roles in this server."
+            )
             return
 
-        whitelisted = [ctx.guild.get_role(role_id) for role_id in whitelisted_roles]
-        whitelisted = [role for role in whitelisted if role is not None]
+        whitelisted = [
+            ctx.guild.get_role(role_id)
+            for role_id in whitelisted_roles
+        ]
+        whitelisted = [
+            role for role in whitelisted if role is not None
+        ]
         whitelisted = sorted(whitelisted, key=lambda x: x.name)
 
         pages = []
         for index in range(0, len(whitelisted), 10):
             entries = whitelisted[index : index + 10]
-            page_content = "\n".join(f"- {role.mention} ({role.id})" for role in entries)
+            page_content = "\n".join(
+                f"- {role.mention} ({role.id})" for role in entries
+            )
             embed: discord.Embed = discord.Embed(
-                title="Whitelisted Roles", description=page_content, color=await ctx.embed_color()
+                title="Whitelisted Roles",
+                description=page_content,
+                color=await ctx.embed_color(),
             )
             pages.append(embed)
 
@@ -180,7 +210,9 @@ class AntiLinks(commands.Cog):
                 await ctx.send("`Members` is a required argument.")
                 return
 
-            async with self.config.guild(ctx.guild).user() as user_list:
+            async with self.config.guild(
+                ctx.guild
+            ).user() as user_list:
                 for member in members:
                     if add_or_remove.lower() == "add":
                         if not member.id in user_list:
@@ -204,18 +236,29 @@ class AntiLinks(commands.Cog):
         whitelisted_users = await guild_config.user()
 
         if not whitelisted_users:
-            await ctx.send("There are no whitelisted users in this server.")
+            await ctx.send(
+                "There are no whitelisted users in this server."
+            )
 
-        whitelisted = [await self.bot.get_or_fetch_user(user_id) for user_id in whitelisted_users]
-        whitelisted = [user for user in whitelisted if user is not None]
+        whitelisted = [
+            await self.bot.get_or_fetch_user(user_id)
+            for user_id in whitelisted_users
+        ]
+        whitelisted = [
+            user for user in whitelisted if user is not None
+        ]
         whitelisted = sorted(whitelisted, key=lambda x: x.name)
 
         pages = []
         for index in range(0, len(whitelisted), 10):
             entries = whitelisted[index : index + 10]
-            page_content = "\n".join(f"- {user.mention} ({user.id})" for user in entries)
+            page_content = "\n".join(
+                f"- {user.mention} ({user.id})" for user in entries
+            )
             embed: discord.Embed = discord.Embed(
-                title="Whitelisted Users", description=page_content, color=await ctx.embed_color()
+                title="Whitelisted Users",
+                description=page_content,
+                color=await ctx.embed_color(),
             )
             pages.append(embed)
 
@@ -226,7 +269,9 @@ class AntiLinks(commands.Cog):
         self,
         ctx: commands.Context,
         add_or_remove: Literal["add", "remove"],
-        channels: commands.Greedy[Union[discord.TextChannel, discord.VoiceChannel]] = None,
+        channels: commands.Greedy[
+            Union[discord.TextChannel, discord.VoiceChannel]
+        ] = None,
     ) -> None:
         """
         Add/remove/list channels to watch.
@@ -238,7 +283,9 @@ class AntiLinks(commands.Cog):
                 await ctx.send("`Channels` is a required argument.")
                 return
 
-            async with self.config.guild(ctx.guild).watching() as watching:
+            async with self.config.guild(
+                ctx.guild
+            ).watching() as watching:
                 for channel in channels:
                     if add_or_remove.lower() == "add":
                         if not channel.id in watching:
@@ -264,17 +311,27 @@ class AntiLinks(commands.Cog):
         watch_list = await guild_config.watching()
 
         if not watch_list:
-            await ctx.send("No channels being watched at this moment.")
+            await ctx.send(
+                "No channels being watched at this moment."
+            )
             return
 
-        channel_list = [ctx.guild.get_channel(channel_id) for channel_id in watch_list]
-        channel_list = [channel for channel in channel_list if channel is not None]
+        channel_list = [
+            ctx.guild.get_channel(channel_id)
+            for channel_id in watch_list
+        ]
+        channel_list = [
+            channel for channel in channel_list if channel is not None
+        ]
         channel_list = sorted(channel_list, key=lambda x: x.name)
 
         pages = []
         for index in range(0, len(channel_list), 10):
             entries = channel_list[index : index + 10]
-            page_content = "\n".join(f"{channel.mention} ({channel.id})" for channel in entries)
+            page_content = "\n".join(
+                f"{channel.mention} ({channel.id})"
+                for channel in entries
+            )
             embed: discord.Embed = discord.Embed(
                 title="AntiLinks Watch List",
                 description=page_content,
@@ -287,7 +344,12 @@ class AntiLinks(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         if isinstance(
-            message.channel, (discord.abc.PrivateChannel, discord.ForumChannel, discord.Thread)
+            message.channel,
+            (
+                discord.abc.PrivateChannel,
+                discord.ForumChannel,
+                discord.Thread,
+            ),
         ):
             return
 
@@ -309,7 +371,9 @@ class AntiLinks(commands.Cog):
         allowed_roles = []
 
         for role in data["role"]:
-            whitelist_role = discord.utils.get(message.author.roles, id=role)
+            whitelist_role = discord.utils.get(
+                message.author.roles, id=role
+            )
             if whitelist_role:
                 allowed_roles.append(whitelist_role)
 
@@ -320,16 +384,24 @@ class AntiLinks(commands.Cog):
                 sentence = message.content.split()
                 for word in sentence:
                     if self._match_url(word):
-                        msg = "**Message Removed in** {} ({})\n".format(
-                            message.channel.mention, message.channel.id
+                        msg = (
+                            "**Message Removed in** {} ({})\n".format(
+                                message.channel.mention,
+                                message.channel.id,
+                            )
                         )
-                        msg += "**Message sent by**: {} ({})\n".format(
-                            message.author.name, message.author.id
+                        msg += (
+                            "**Message sent by**: {} ({})\n".format(
+                                message.author.name, message.author.id
+                            )
                         )
-                        msg += "**Message content**:\n- {}".format(message.content)
+                        msg += "**Message content**:\n- {}".format(
+                            message.content
+                        )
                         if message_channel:
-                            ctx = await self.bot.get_context(message)
-                            await ctx.maybe_send_embed(msg)
+                            await message_channel.send(
+                                box(msg, lang="py")
+                            )
                         await message.delete()
             except Exception as e:
                 if message_channel:
